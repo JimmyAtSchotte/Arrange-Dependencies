c284910cac9203f1719d68957ca081df7290415a
Martin Björkvall <martin.bjorkvall@rekylsystems.se>
2024-02-21 15:07:00 +0100

    Removed obsolete methods as stated in code.

--- ArrangeDependencies.Autofac/ArrangeDependencies.cs
+++ ArrangeDependencies.Autofac/ArrangeDependencies.cs
@@ -1,78 +1,20 @@
 ﻿using ArrangeDependencies.Core.Interfaces;
-using ArrangeDependencies.Autofac.Extensions;
 using Autofac;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
-using Moq;
-using ArrangeDependencies.Autofac.Helpers;
 
 namespace ArrangeDependencies.Autofac
 {
-    public class ArrangeDependencies : IArrangeDependencies
-    {
-        private IContainer _container;
+	public class ArrangeDependencies : IArrangeDependencies
+	{
+		private readonly IContainer _container;
 
-        internal ArrangeDependencies(IContainer container)
-        {
-            _container = container;
-        }
+		internal ArrangeDependencies(IContainer container)
+		{
+			_container = container;
+		}
 
-
-        public TInterface Resolve<TInterface>()
-        {
-            return _container.Resolve<TInterface>();
-        }
-
-
-        [Obsolete("This method has been deprecated and will be removed in 2.0. Use Arrange.Dependencies() instead.")]
-        public static IArrangeDependencies Config(Action<IArrangeBuilder<ContainerBuilder>> config = null)
-        {
-            var arrangeBuilder = new ArrangeBuilder();
-            config?.Invoke(arrangeBuilder);
-
-            var containerBuilder = arrangeBuilder.Build(); 
-            var container = containerBuilder.Build();
-
-            return new ArrangeDependencies(container);
-        }
-
-
-        /// <summary>
-        /// Looosly configure T dependencies, if you don't define the dependency it will default to a mock of the dependency
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="config"></param>
-        /// <returns></returns>
-        /// 
-        [Obsolete("This method has been deprecated and will be removed in 2.0. Use Arrange.Dependencies<TInterface, TImplementation>() instead.")]
-        public static IArrangeDependencies Config<TInterface, TImplementation>(Action<IArrangeBuilder<ContainerBuilder>> config = null) 
-            where TInterface : class
-            where TImplementation : class, TInterface
-        {
-            var arrangeBuilder = new ArrangeBuilder();
-            config?.Invoke(arrangeBuilder);
-
-            arrangeBuilder.UseImplementation<TInterface, TImplementation>();
-                       
-            var containerBuilder = arrangeBuilder.Build();
-            containerBuilder.Register((context) =>
-            {
-                var constructors = typeof(TImplementation).GetConstructors();
-                var parameters = constructors.SelectMany(x => x.GetParameters()).Where(x => x.ParameterType.IsInterface).Distinct();
-                var args = new List<object>();
-
-                foreach (var parameter in parameters)
-                    args.Add(context.ResolveOptional(parameter.ParameterType) ?? MockHelper.CreateMock(parameter.ParameterType));
-
-                return Activator.CreateInstance(typeof(TImplementation), args.ToArray()) as TImplementation;
-            }).As<TInterface>().IfNotRegistered(typeof(TInterface));
-
-            var container = containerBuilder.Build();
-                       
-            return new ArrangeDependencies(container);
-        }      
-       
-    }
+		public TInterface Resolve<TInterface>()
+		{
+			return _container.Resolve<TInterface>();
+		}
+	}
 }
